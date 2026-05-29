8b8e24d6e92921d3d6f5dbcd9a54aef5d04c72c4
Camila <camila.ianni@davinci.edu.ar>
2026-05-29 11:26:37 -0300

    fix: update module exports to PancakeSwapMonitor

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,7 +2,7 @@
 
 from .arbitrage_engine import ArbitrageEngine
 from .crypto_feed import CryptoFeed
-from .polymarket_monitor import PolymarketMonitor
+from .pancakeswap_monitor import PancakeSwapMonitor
 from .web3_executor import Web3Executor
 
-__all__ = ["ArbitrageEngine", "CryptoFeed", "PolymarketMonitor", "Web3Executor"]
+__all__ = ["ArbitrageEngine", "CryptoFeed", "PancakeSwapMonitor", "Web3Executor"]
